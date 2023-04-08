81c8c2e7080a5ff29ddfe74394c7fba83b4fe21a
keplib <keplib@gmail.com>
2023-04-08 16:50:10 +0200

    Initial commit

---
+++ CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Balázs Képli on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
